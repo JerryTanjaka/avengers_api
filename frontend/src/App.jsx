618b4eb1a56bf4cc0cf494ef3c6f6ce766b7e80e
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-07-29 18:04:58 +0300

    style : change background color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,94 +1,89 @@
 import { useEffect, useState } from "react";
 import CharacterCard from "./components/CharacterCard";
 import CharacterForm from "./components/CharacterForm";
-import { Navbar } from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [newCharacter, setNewCharacter] = useState({ name: "", realName: "", universe: "" });
-  const [showForm, setShowForm] = useState(false); // pour la modale
+    const [characters, setCharacters] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [newCharacter, setNewCharacter] = useState({ name: "", realName: "", universe: "" });
+    const [showForm, setShowForm] = useState(false); // pour la modale
 
-  const API_URL = "http://localhost:5000/characters";
+    const API_URL = "http://localhost:5000/characters";
 
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then(setCharacters)
-      .catch((err) => console.error("Erreur API:", err));
-  }, []);
+    useEffect(() => {
+        fetch(API_URL)
+            .then((res) => res.json())
+            .then(setCharacters)
+            .catch((err) => console.error("Erreur API:", err));
+    }, []);
 
-  const handleAdd = () => {
-    fetch(API_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newCharacter),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setCharacters([...characters, data.character]);
-        setNewCharacter({ name: "", realName: "", universe: "" });
-        setShowForm(false); // fermer le formulaire
-      });
-  };
+    const handleAdd = () => {
+        fetch(API_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newCharacter),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setCharacters([...characters, data.character]);
+                setNewCharacter({ name: "", realName: "", universe: "" });
+                setShowForm(false); // fermer le formulaire
+            });
+    };
 
-  const handleDelete = (id) => {
-    fetch(`${API_URL}/${id}`, { method: "DELETE" }).then(() => {
-      setCharacters(characters.filter((char) => char.id !== id));
-    });
-  };
+    const handleDelete = (id) => {
+        fetch(`${API_URL}/${id}`, { method: "DELETE" }).then(() => {
+            setCharacters(characters.filter((char) => char.id !== id));
+        });
+    };
 
-  const handleUpdate = (id) => {
-    const updatedName = prompt("Nouveau nom ?");
-    if (!updatedName) return;
-    fetch(`${API_URL}/${id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name: updatedName }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setCharacters(characters.map((char) => (char.id === id ? data.character : char)));
-      });
-  };
+    const handleUpdate = (id) => {
+        const updatedName = prompt("Nouveau nom ?");
+        if (!updatedName) return;
+        fetch(`${API_URL}/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name: updatedName }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setCharacters(characters.map((char) => (char.id === id ? data.character : char)));
+            });
+    };
 
-  const filtered = characters.filter((char) => char.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    const filtered = characters.filter((char) => char.name.toLowerCase().includes(searchTerm.toLowerCase()));
 
-  return (
-    <div className="min-h-screen overflow-x-hidden bg-gray-200">
-      <Navbar searchTerm={searchTerm} onChange={setSearchTerm} />
+    return (
+        <div className="min-h-screen overflow-x-hidden bg-blue-950/35">
+            <Navbar searchTerm={searchTerm} onChange={setSearchTerm} />
 
-      <div className="p-6">
-        <div className="mb-4 flex justify-center">
-          <button onClick={() => setShowForm(true)} className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700">
-            Ajouter un Avenger
-          </button>
-        </div>
+            <div className="p-6">
+                <div className="mb-4 flex justify-center">
+                    <button onClick={() => setShowForm(true)} className="rounded bg-purple-900 px-4 py-2 text-white hover:bg-purple-950">
+                        Ajouter un Avengers
+                    </button>
+                </div>
 
-        {/* Formulaire en modale */}
-        {showForm && (
-          <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs backdrop-brightness-50">
-            <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
-              <CharacterForm
-                newCharacter={newCharacter}
-                onChange={setNewCharacter}
-                onAdd={handleAdd}
-                onCancel={() => setShowForm(false)} 
-              />
+                {/* Formulaire en modale */}
+                {showForm && (
+                    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs backdrop-brightness-50">
+                        <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
+                            <CharacterForm newCharacter={newCharacter} onChange={setNewCharacter} onAdd={handleAdd} onCancel={() => setShowForm(false)} />
+                        </div>
+                    </div>
+                )}
+
+                <div className="mx-auto mb-6 grid gap-4 rounded-2xl bg-white p-4 shadow-[0_0_12px_rgba(0,0,0,0.45)] md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                    {filtered.map((char) => (
+                        <CharacterCard key={char.id} character={char} onDelete={handleDelete} onUpdate={handleUpdate} />
+                    ))}
+                </div>
             </div>
-          </div>
-        )}
-
-        <div className="mx-auto mb-6 grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {filtered.map((char) => (
-            <CharacterCard key={char.id} character={char} onDelete={handleDelete} onUpdate={handleUpdate} />
-          ))}
+            <Footer />
         </div>
-      </div>
-      <Footer/>
-    </div>
-  );
+    );
 }
 
 export default App;
--- frontend/src/components/CharacterCard.jsx
+++ frontend/src/components/CharacterCard.jsx
@@ -1,6 +1,6 @@
 function CharacterCard({ character, onDelete, onUpdate }) {
     return (
-        <div className="flex text-white items-center justify-between rounded-xl bg-gray-800 p-4 shadow-lg shadow-gray-950/30 transition duration-100 ease-in-out hover:scale-105">
+        <div className="flex text-white items-center justify-between rounded-xl bg-gray-800 p-4 shadow-[0_0_8px_rgba(0,0,0,0.6)] transition duration-100 ease-in-out hover:scale-105">
             <div>
                 <h2 className="text-xl font-bold">{character.name}</h2>
                 <p className="text-sm text-gray-200">{character.realName || character.alias}</p>
@@ -11,7 +11,7 @@ function CharacterCard({ character, onDelete, onUpdate }) {
                     Modifier
                 </button>
 
-                <button onClick={() => onDelete(character.id)} className="transform rounded bg-red-600 px-3 py-1 text-white shadow-md transition duration-100 ease-in-out  hover:bg-red-00 active:scale-95">
+                <button onClick={() => onDelete(character.id)} className="transform rounded bg-red-600 px-3 py-1 text-white shadow-md transition duration-100 ease-in-out  hover:bg-red-400 active:scale-95">
                     Supprimer
                 </button>
             </div>
